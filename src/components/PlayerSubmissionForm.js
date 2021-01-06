2b37a7bc07a5efa1c090cfcecdb40b2600baadd1
Kayla Johnson <kayla.elizabeth89@gmail.com>
2021-01-05 22:31:46 -0800

    Renamed test file to work properly, added UseState to Playersubmission form js

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,47 +1,58 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = (props) => {
+  const [submission, setSubmission] = useState ({
+    adj1: '',
+    noun1: '',
+    adverb: '',
+    verb: '',
+    adj2: '',
+    noun2: '',
+  })
+
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{ }</h3>
 
       <form className="PlayerSubmissionForm__form" >
 
         <div className="PlayerSubmissionForm__poem-inputs">
+                  
+                  <p>The</p>
 
                   <input
+                  name = "adj1"
                   placeholder="adjective"
                   type="text" />,
 
                   <input
+                  name = "noun1"
                   placeholder="noun"
                   type="text" />,
 
                   <input
+                  name = "adverb"
                   placeholder="adverb"
                   type="text" />,
 
                   <input
+                  name = "verb"
                   placeholder="verb"
-                  type="text" />
+                  type="text" />  
 
                   <p>the</p>
 
                   <input
+                  name = "adj2"
                   placeholder="adjective"
                   type="text" />
 
                   <input
+                  name = "noun2"
                   placeholder="noun"
                   type="text" />
-
-
-            
-          
-          
 
         </div>
 
